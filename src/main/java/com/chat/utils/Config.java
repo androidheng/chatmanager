6f9af83d455a6bfbbf2741bf4278244fc415a140
androidheng <19930425zh>
2019-05-08 11:58:02 +0800

    1

---
+++ src/main/java/com/chat/utils/Config.java
@@ -0,0 +1,9 @@
+package com.chat.utils;
+
+public class Config {
+//    public static String URL="http://192.168.1.105:8080/";
+    //本机 homeip
+    //public static String URL="http://192.168.1.102:8080/";
+
+    public static String URL="http://192.168.0.178:8080/";
+}
